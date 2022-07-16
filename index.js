532507468b5794ee7c5de0a94c331101e928fac2
Zachary-Maddox <z19s96p@aol.com>
2022-07-16 00:29:36 -0500

    finalized read me , attached link to live video , code is complete

--- index.js
+++ index.js
@@ -2,77 +2,68 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
-const { stringify } = require("querystring");
-
 // TODO: Create an array of questions for user input
 
 const questions = [
   {
-  name:"Description",
-  type:"input",
-  message:"Add a Description"
+    name: "title",
+    type: "input",
+    message: "What is the projects title name?",
 },
-{
-name:"Table Of Contents",
-type:"input",
-message:"Add a table of contents."
-},
-{
-  name:"Installation",
-  type:"input",
-  message:"Installations NPM"
-},
-{
-  name:"Usage",
-  type:"input",
-  message:"Run node index answer all prompts?"
-},
-{
-  name:"License",
-  type:"list",
-  message:" Choose a liscence type",
-  choices:["MIT","ISC"]
-},
-{
-name:"Contributing",
-type:"input",
-message:"Who is Contributing?"
-},
-{
-  name:"tests",
-  type:"input",
-  message:"IDK"
-},
-{
-  name:"Questions",
-  type:"input",
-  message:"Contact me with any questions."
-},
-{
-  name:"title",
-  type:"input",
-  message:"Write title name"
-}
-]
+      {
+      name:"description",
+      type:"input",
+      message:"Add a Description of project."
+    },
+    {
+      name:"installation",
+      type:"input",
+      message:"What are your Installation requirements?"
+    },
+    {
+      name:"usage",
+      type:"input",
+      message:"What are the usage intructions?"
+    },
+    {
+      name:"license",
+      type:"list",
+      message:" Choose a liscence type",
+      choices:["MIT","ISC","none"]
+    },
+    {
+    name:"contributing",
+    type:"input",
+    message:"How can you Contribute to the project?"
+    },
+    {
+      name:"tests",
+      type:"input",
+      message:"How do you test your project?"
+    },
+    {
+      name:"questions",
+      type:"input",
+      message:"What is your contact info?"
+    },
+];
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer
-.prompt(questions)
-.then((data) => {
-  console.log(data)
-});
+    inquirer.prompt(questions).then((userAnswers) => {
+        console.log(userAnswers);
 
-
-
-  // generateMarkdown(reponses)
+        const readmeTemp = generateMarkdown(userAnswers);
+        console.log(readmeTemp);
+        writeToFile("./dist/README.md", readmeTemp);
+    });
 }
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile('README.md', data, (err) =>
-  err ? console.error(err) : console.log(response)
-);
+    fs.writeFile(fileName, data, (err) => {
+        if (err) console.error(err);
+    });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,26 +1,40 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}`
-  `#${data.description}`
-  `#${data.tableofcontents}`
-  `#${data.Questions}`
-  `#${data.License}`
-  `#${data.Usage}`
-  `#${data.Installation}`
-  `#${data.Contributing}`;
+function generateMarkdown(userAnswers) {
+    return `# ${userAnswers.title}
+
+  ## Description
   
+${userAnswers.description}
+  
+  ## Table of contents
+  
+  - [license](#license)
+- [installation](#installation)
+- [usage](#usage)
+- [contributing](#contributing)
+- [questions](#questions)
+
+  
+  
+  ## License
+  
+  <img src = "https://img.shields.io/badge/license-${userAnswers.license}-blue" />
+
+  ## Installation
+
+  ${userAnswers.installation}
+
+## Usage
+
+${userAnswers.usage}
+
+## Contributing
+
+${userAnswers.contributing}
+
+## Questions
+
+${userAnswers.questions}`;
 }
 
 module.exports = generateMarkdown;
